a07b7862dd3fff1ff2af4d2ceafb923d7c4be42f
kzhxxn <kzhxxn@gmail.com>
2021-10-12 10:56:20 +0900

    addpost

--- wish/src/components/Post.js
+++ wish/src/components/Post.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import {Grid, Text, Image, Button} from "../elements"
 import styled from "styled-components";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Post = (props) => {
 
-    const  {image_url,user_profile,insert_dt} = props;
+    const  {image_url,user_profile, insert_dt} = props;
     return (
         <PostContainer>
             <Grid padding="16px" bg="#ffffff" margin="8px 0px">
                 <Grid is_flex>
-                    <div >
+                    <Profile>
                         <Image shape="circle" src={user_profile} />
                         <Text bold>{props.user_info.user_name}</Text>
-                    </div>
+                    </Profile>
                     <div>                        
-                        <Button width="20px">수정</Button>
-                        <Button width="20px">삭제</Button>
+                        <EditIcon >수정</EditIcon>
+                        <DeleteIcon >삭제</DeleteIcon>
                     </div>
                 </Grid>
-                <Grid is_flex>
-                    <Image shape="rectangle" src={image_url} />
+                <Grid>
+                    <Image shape="rectangle" src={image_url}  />
                 </Grid>
                 <Grid is_flex>
                     <Text>{props.content}</Text>
                     <Text>{props.insert_dt}</Text>
                 </Grid>
                 <Grid is_flex>
-                    <Text>댓글{props.comment_cnt}개 모두 보기</Text>
-                    <Text>{props.is_like}</Text>
+                    <Text bold>댓글{props.comment_cnt}개 모두 보기</Text>
+                    <FavoriteBorderIcon>{props.is_like}</FavoriteBorderIcon>
                 </Grid>
-            
             </Grid>
         </PostContainer>
         
@@ -43,7 +45,7 @@ Post.defaultProps = {
     },
     image_url: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
     content: "블라블라",
-    insert_dt: 2021-10-11, 
+    insert_dt: "2021-10-11 10:00:00", 
     like_cnt: 10,
     comment_cnt : 10,
     is_like: false,
@@ -52,24 +54,21 @@ Post.defaultProps = {
 const PostContainer = styled.div`
   background-color: white;
   width: 60vw;
-  max-width: 450px;
-  justify-content: space-between;
+  max-width: 400px;
   margin: auto;
   margin-top: 70px;
   margin-bottom: 30px;
   border-radius: 5px;
-  padding-bottom : 20px;
   box-shadow: 0 3px 6px rgba(0,0,0,0.12), 0 2px 5px rgba(0,0,0,0.24);
-  /* @media (max-width: 750px){
-    width: 80vw;
+  @media (max-width: 750px){
+    width: 80%;
   }
-  @media (max-width: 350px){
-    width: 100vw;
-  } */
+  @media (max-width: 450px){
+    width: 100%;
 `
 
-const PostHeader = styled.div`
- justify-content: space-between;
+const Profile = styled.div`
+ display: flex;
 `;
 
 export default Post;
--- wish/src/pages/Postlist.js
+++ wish/src/pages/Postlist.js
@@ -5,36 +5,11 @@ import styled from 'styled-components'
 
 const PostList = () => {
     return (
-        <Warp>
             <Grid>
                 <Post/>
             </Grid>
-            <Grid>
-                <Post/>
-            </Grid>
-            <Grid>
-                <Post/>
-            </Grid>
-            <Grid>
-                <Post/>
-            </Grid>
-            <Grid>
-                <Post/>
-            </Grid>
-            <Grid>
-                <Post/>
-            </Grid>
-        </Warp>
     )
 }
 
-const Warp = styled.div`
-max-width: 1200px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
-}
-`;
 
 export default PostList
