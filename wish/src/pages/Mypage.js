2e6c635237cb7826a872895b6b450877dba78915
erionRK621 <jhkim62134@naver.com>
2021-10-16 01:50:16 +0900

    마이페이지 수정 전

--- wish/src/pages/Mypage.js
+++ wish/src/pages/Mypage.js
@@ -1,34 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Grid, Image, Text, Button } from "../elements";
 
-const Mypage = () => {
+import { history } from "../redux/configureStore";
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators as postCreators } from "../redux/modules/mypage";
+
+const Mypage = (props) => {
+  console.log(props);
+  const dispatch = useDispatch();
+  const { history } = props;
+  //포스트리스트값 불러오기
+  const post_list = useSelector((state) => state.post.list);
+  console.log(post_list);
+  // const user_info = useSelector((state) => state.user);
+
+  // console.log(user_info);
+
+  useEffect(() => {
+    dispatch(postCreators.getPostsDB());
+  }, []);
+
   return (
-    <Grid >
+    <Grid>
       <Grid is_flex padding="20px" backgroundColor="#e6f3f7">
         <Image shape="circle" size={85}></Image>
         <Text bold size="20px">
-          NICK NAME
+          {}
         </Text>
         <Text bold size="20px">
-          10개
+          {}개
         </Text>
       </Grid>
       <GridWrap>
         <PostGrid>
-          <Image shape="rectangle" />
-        </PostGrid>
-        <PostGrid>
-          <Image shape="rectangle" />
-        </PostGrid>
-        <PostGrid>
-          <Image shape="rectangle" />
-        </PostGrid>
-        <PostGrid>
-          <Image shape="rectangle" />
+          {post_list.map((p, idx) => {
+            return <MyPostImg key={idx} {...p} />;
+          })}
         </PostGrid>
       </GridWrap>
     </Grid>
+  );
+};
+
+const MyPostImg = (props) => {
+  console.log(props);
+  const { post_id, image_url } = props;
+
+  return (
+    <>
+      <Image
+        shape="rectangle"
+        src={image_url}
+        onClick={() => {
+          history.push(`/api/postings/${post_id}`);
+        }}
+      />
+    </>
   );
 };
 
--- wish/src/redux/modules/mypage.js
+++ wish/src/redux/modules/mypage.js
@@ -13,68 +13,59 @@ const SET_POSTS = "SET_POSTS";
 const setPosts = createAction(SET_POSTS, (post_list) => ({ post_list }));
 
 const initialState = {
-    list: [],
+  list: [],
+};
+
+const initialPost = {
+  user_info: {
+    id: 0,
+    user_name: "wish",
+    user_profile:
+      "https://images.unsplash.com/photo-1540331547168-8b63109225b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=719&q=80",
+  },
+  image_url:
+    "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+  contents: "",
+  like_cnt: 0,
+  comment_cnt: 10,
+  insert_dt: moment().format("YYYY-MM-DD hh:mm:ss"),
+};
+
+const getPostsDB = () => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .getMyPost()
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        if (res.data.token) {
+          window.localStorage.setItem("token", JSON.stringify(res.data.token));
+        }
+        dispatch(setPosts(res.data));
+      })
+      .catch((err) => {
+        //요청이 정상적으로 안됬을때 수행
+        console.log(err, "에러");
+      });
   };
-  
+};
 
-  const initialPost = {
-    user_info: {
-      id: 0,
-      user_name: "wish",
-      user_profile:
-        "https://images.unsplash.com/photo-1540331547168-8b63109225b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=719&q=80",
-    },
-    image_url:
-      "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
-    contents: "",
-    like_cnt: 0,
-    comment_cnt: 10,
-    insert_dt: moment().format("YYYY-MM-DD hh:mm:ss"),
-  };
+export default handleActions(
+  {
+    [SET_POSTS]: (state, action) =>
+      produce(state, (draft) => {
+        // undifined는 값이 잘넘어가고있다. 값이 나올경우 어딘가에 문제가 있는것
 
+        console.log(action.payload.postings);
+        draft.list = action.payload.post_list.postings;
+      }),
+  },
+  initialState
+);
 
-  const getPostsDB = () => {
-    return function (dispatch, getState, { history }) {
-      apis
-        .getPost()
-        .then((res) => {
-          console.log(res);
-          console.log(res.data);
-          if (res.data.token) {
-            window.localStorage.setItem("token", JSON.stringify(res.data.token));
-          }
-          dispatch(setPost(res.data));
-        })
-        .catch((err) => {
-          //요청이 정상적으로 안됬을때 수행
-          console.log(err, "에러");
-        });
-    };
-  };
+const actionCreators = {
+  setPosts,
+  getPostsDB,
+};
 
-
-
-  export default handleActions(
-    {
-      [SET_POST]: (state, action) =>
-        produce(state, (draft) => {
-          // undifined는 값이 잘넘어가고있다. 값이 나올경우 어딘가에 문제가 있는것
-  
-          console.log(action.payload.postings);
-          draft.list = action.payload.post_list.postings;
-          
-        }),
-  
-      
-        }),
-    },
-    initialState
-  );
-
-  const actionCreators = {
-    setPosts,
-    getPostsDB,
-    
-  };
-  
-  export { actionCreators };
+export { actionCreators };
